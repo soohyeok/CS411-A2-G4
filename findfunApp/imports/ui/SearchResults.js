18f1186b6d0309bb0f42df327511ef5ddda53aba
Shreenidhi Jayaram <shreej@bu.edu>
2018-12-10 10:29:11 -0500

    update searchresults.js to display weatherinfo

--- findfunApp/imports/ui/SearchResults.js
+++ findfunApp/imports/ui/SearchResults.js
@@ -5,6 +5,7 @@ import Navbar from './Navbar.js';
 import ActivityList from './ActivityList.js';
 import YelpList from './YelpList.js';
 import TicketmasterList from './TicketmasterList.js';
+import WeatherInfo from './WeatherInfo.js';
 
 export default class SearchResults extends React.Component {
   constructor(props) {
@@ -45,7 +46,7 @@ export default class SearchResults extends React.Component {
         <Navbar />
         <div className="hero-body">
           <div className="container">
-            {/* <WeatherList city={this.props.params.city} /> */}
+                <WeatherInfo city={this.props.params.city}/>
             <div className="box">
               <h2 className="title is-2 has-text-grey-dark">Find your fun</h2>
               <h4 className="subtitle is-4 has-text-grey-dark">Browse our recommended activites and bookmark your favorites to have a {this.props.params.time} of fun</h4>
